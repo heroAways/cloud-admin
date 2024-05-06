8571fdfd9bb8cf101ac6d8105b6f1a6bb1b71d9e
lyf <2929403059@qq.com>
2024-05-06 19:37:52 +0800

    mockjs练习获取数据成功

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import SvgIcon from './components/SvgIcon.vue';
+import request from './utils/request';
+const req = ()=>{
+  // request.post('/login').then(res=>{
+  //   console.log(res);
+  // })
+  // request.get('/banner').then(res=>{
+  //   console.log(res);
+    
+  // })
+}
 </script>
 
 <template>
   <SvgIcon icon="Loading"></SvgIcon>
+  <button @click="req">点击</button>
   <RouterView />
 </template>
 
